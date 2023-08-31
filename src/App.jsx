09c022eb0db2fc74f76fdc9492c3fdea7aa70a96
Collins Samson Kipepe <64184560+Cosaki24@users.noreply.github.com>
2023-08-31 13:35:24 +0000

    modified:   src/App.jsx 	modified:   src/Components/About.jsx 	modified:   src/Components/Home.jsx 	modified:   src/Components/Portfolio.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Collins Kipepe",
+  title: "Software Developer",
+  email: "cskipepe@gmail.com",
+  gitHub: "Cosaki24",
+  instagram: "_cosakiworld",
+  linkedIn: "Collins Kipepe",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "",
+  youTube: "",
 };
 
 const primaryColor = "#4E567E";
