bb7dd41edcb6e34d58d63d8466ebba324ae7a136
Collins Samson Kipepe <64184560+Cosaki24@users.noreply.github.com>
2023-09-05 14:51:36 +0000

    modified:   package.json 	modified:   src/Components/Portfolio.jsx

--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -31,28 +31,22 @@ const imageAltText = "A shoe artisan";
  */
 const projectList = [
   {
-    title: "10 Things To Know About Azure Static Web Apps 🎉",
+    title: "Pathfinder",
     description:
-      "Collaboration to create a beginner friendly article to help explain Azure Static Web Apps and tooling to get started.",
-    url: "https://dev.to/azure/10-things-to-know-about-azure-static-web-apps-3n4i",
+      "An intelligent Robot which finds paths available in a grid while avoiding obstacles.",
+    url: "https://github.com/Cosaki24/PathFinder",
   },
   {
-    title: "Web Development for Beginners",
+    title: "Fantasy Premier League Squad Selector",
     description:
-      "Contributed sketch note imagery to accompany each lesson. These help provide visual representation of what is being taught.",
-    url: "https://github.com/microsoft/web-dev-for-beginners",
+      "A tool to select fantasy premier league squad using linear programming",
+    url: "https://github.com/Cosaki24/Fantasy-premier-league",
   },
   {
-    title: "My Resume Site",
+    title: "IoT Automatic Weather Station",
     description:
-      "Created from Microsoft's resume workshop and deployed to GitHub pages. Includes my experience and design abilities.",
-    url: "https://github.com/microsoft/workshop-library/tree/main/full/build-resume-website",
-  },
-  {
-    title: "GitHub Codespaces and github.dev",
-    description:
-      "Video interview to explain when to use GitHub.dev versus GitHub Codespaces, and how best to use each tool.",
-    url: "https://www.youtube.com/watch?v=c3hHhRME_XI",
+      "Automatic Weather Station using Arduino, React and Python",
+    url: "https://github.com/Cosaki24/weather-station",
   },
 ];
 
